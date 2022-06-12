e9e192f1162fecafc80b64716a61c920084a4233
nasubi <r916nis@gmail.com>
2022-06-12 11:56:51 +0900

    pinia setup

--- src/components/nasubi.vue
+++ src/components/nasubi.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { NButton } from 'naive-ui'
+import { useCounterStore } from '../store/counter';
 
+const store = useCounterStore();
+store.count++;
+store.increment(2);
+store.decrement(2);
 </script>
 
 <template>
     <div>
-        <n-button>naive-ui</n-button>
+        <n-button>naive-ui</n-button><br>
+        <h1>Count is {{store.count }}</h1>
+        <h2>DoubleCount is {{ store.doubleCount }}</h2>
+        <button @click="store.increment(1)">Increment</button>
+        <button @click="store.decrement(1)">Decrement</button>
+        <button @click="store.waitAndAdd">Wait + Increment</button>
     </div>
 </template>
 
 <style>
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(createPinia())
+.mount('#app')
